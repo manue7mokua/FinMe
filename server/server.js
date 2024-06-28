0d1ed30d9477025b9a7e6cd8b656618d8bbf638c
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-06-28 16:05:17 -0700

    Setup endpoint to allow users to sign up for an account
    Route verifies if user exists in the database before encrypting their password and storing it the user's information in the database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 var cors = require('cors');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const salt = bcrypt.genSaltSync(10);
 
-// const { PrismaClient } = require('@prisma/client');
-// const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 const PORT = 3000
 
 app.use(cors())
@@ -12,5 +15,38 @@ app.use(express.json({ extended: false }));
 app.get('/home', (req, res) => {
     res.send(`Hello`);
 })
+
+app.post('/users/signup', async (req, res) => {
+    const { firstName, lastName, email, password} = req.body;
+
+    // Have a check to verify user is in database
+    const existingUser = await prisma.student.findUnique({
+        where: {
+            email
+        }
+    });
+    if (existingUser) {
+        return res.status(400).send({ message: `Username is taken. Try logging in`})
+    }
+
+    console.log(req.body);
+    // If new user, hash password using bcrypt
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    // Add new user to the database
+    const newUser = await prisma.student.create({
+        data: {
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // Generate a jwt token for the new user
+    const token = jwt.sign(newUser, 'awognawole3280843halegn');
+    res.send({ token });  // Send token to the client
+})
+
 
 app.listen(PORT);
