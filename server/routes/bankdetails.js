d0cec64ffcd75cc20b7af3c24c16ab2bca47f707
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-02 14:51:13 -0700

    Route to enable user to add bank account details
    Changed my accountNumber data type to a string instead of integer due to big input values for integer type
    
    Extract specific user id from route url to match account to logged in user
    
    Extract year and month from user card expiry date input and use JS Date methods to convert to ISO 8601 string
    
    Create a new user account and send 200 status code if process was successful

--- server/routes/bankdetails.js
+++ server/routes/bankdetails.js
@@ -1,1 +1,48 @@
+const { PrismaClient } = require(('@prisma/client'));
+const prisma = new PrismaClient();
+var cors = require('cors');
 
+const express = require('express');
+const router = express.Router();
+
+// Route to add bank account to database
+router.post('/:id/addAccount', async(req, res) => {
+    const { accountNumber, accountName, cardExpiresOn } = req.body;
+    // Get userid from url
+    const studentId = parseInt(req.params.id);
+
+    // Extract year from expiry date
+    let expiryYear = cardExpiresOn.split(' ')[0];
+
+    // Extract month from expiry date
+    let expiryMonth = cardExpiresOn.split(' ')[1];
+
+    // Get the last day of month
+    const lastDayofMonth = (year, month) => {
+        return new Date(year, month, 0).getDate();
+    }
+
+    const setCardExpiryDate = (year, month) => {
+        const lastDay = lastDayofMonth(expiryYear, expiryMonth);
+        const expiryDate = new Date(year, month - 1, lastDay);
+
+        return expiryDate.toISOString();
+    }
+
+    const cardExpiryDate = setCardExpiryDate(expiryYear, expiryMonth);
+
+    await prisma.account.create({
+        data: {
+            studentId,
+            accountNumber,
+            accountName,
+            cardExpiryDate
+        }
+    })
+
+    // Return json of newly added card data
+    res.status(200).send('Great! New card added!');
+})
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 var cors = require('cors');
 const userRoutes = require('./routes/users.js')
+const accountRoutes = require('./routes/bankdetails.js')
 
 const PORT = 3000
 
@@ -10,5 +11,8 @@ app.use(express.json({ extended: false }));
 
 // Use user routes
 app.use('/users', userRoutes);
+
+// Use account routes
+app.use('/users', accountRoutes);
 
 app.listen(PORT);
