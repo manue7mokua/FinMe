3144a4ebec174e3d87fd037954cf49b6a11f434f
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-02 15:37:22 -0700

    Route to delete a user bank account
    Modified endpoint URL to have logged in user id and specific account id
    
    Check if specific account exists under user in database using student id and account id
    
    Send error message to user if account does not exist
    
    If account exists, delete account from database using it's name and id
    
    Respond with 200 status if account deletion was successful

--- server/routes/bankdetails.js
+++ server/routes/bankdetails.js
@@ -17,28 +17,33 @@ router.post('/:id/addAccount', async(req, res) => {
     // Extract month from expiry date
     let expiryMonth = cardExpiresOn.split(' ')[1];
 
-    // Get the last day of month
-    const lastDayofMonth = (year, month) => {
-        return new Date(year, month, 0).getDate();
-    }
-
-    const setCardExpiryDate = (year, month) => {
-        const lastDay = lastDayofMonth(expiryYear, expiryMonth);
-        const expiryDate = new Date(year, month - 1, lastDay);
-
-        return expiryDate.toISOString();
-    }
-
-    const cardExpiryDate = setCardExpiryDate(expiryYear, expiryMonth);
-
-    await prisma.account.create({
-        data: {
-            studentId,
-            accountNumber,
-            accountName,
-            cardExpiryDate
+    try {
+         // Get the last day of month
+        const lastDayofMonth = (year, month) => {
+            return new Date(year, month, 0).getDate();
         }
-    })
+
+        const setCardExpiryDate = (year, month) => {
+            const lastDay = lastDayofMonth(expiryYear, expiryMonth);
+            const expiryDate = new Date(year, month - 1, lastDay);
+
+            return expiryDate.toISOString();
+        }
+
+        const cardExpiryDate = setCardExpiryDate(expiryYear, expiryMonth);
+
+        await prisma.account.create({
+            data: {
+                studentId,
+                accountNumber,
+                accountName,
+                cardExpiryDate
+            }
+        })
+    } catch(err) {
+        res.status(500).send('Server error!');
+    }
+
 
     // Return json of newly added card data
     res.status(200).send('Great! New card added!');
@@ -47,14 +52,54 @@ router.post('/:id/addAccount', async(req, res) => {
 router.get('/:id/accountInfo', async (req, res) => {
     const studentId = parseInt(req.params.id);
 
-    // Get all user accounts
-    const bankAccounts = await prisma.account.findMany({
-        where: {
-            studentId
-        }
-    });
+    try {
+        // Get all user accounts
+        const bankAccounts = await prisma.account.findMany({
+            where: {
+                studentId
+            }
+        });
+    } catch (err) {
+        res.status(500).send('Server error!')
+    }
+
 
     res.json(bankAccounts);
+})
+
+// Route to delete a user bank account
+router.delete('/:id/deleteAccount/:accountId', async (req, res) => {
+    const { accountName } = req.body;
+    const studentId = parseInt(req.params.id);
+    const accountId = parseInt(req.params.accountId);
+
+    try {
+       // Find account specified by accountName
+        const account = await prisma.account.findUnique({
+            where: {
+                studentId,
+                id: accountId,
+                accountName
+            }
+        })
+
+        if (!account) {
+            return res.status(404).send('That account does not exist!');
+        };
+
+        await prisma.account.delete({
+            where: {
+                id: accountId,
+                accountName
+            }
+        });
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error!');
+    }
+
+    // Success if process completed without errors
+    res.status(200).send('Account deleted successfully! :)');
 })
 
 module.exports = router;
