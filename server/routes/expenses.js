330999cdb63eb7b1bf97b733b3e29cfccb24b8a9
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-03 11:25:52 -0700

    Added route to record a user's expenses
    Creating new post route '/:id/addExpense' to allow user to add an expense
    
    Validation of the 'dateofExpense' to ensure it is provided with user input.
    
    Parsing and formatting of the 'dateofExpense' to ISO 8601 format
    
    Retrieval of the bank account using 'accountName' and 'studentId'.
    
    Creation of a new expense record with all provided attributes from the request body

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -5,6 +5,46 @@ var cors = require('cors');
 const express = require('express');
 const router = express.Router();
 
+// Route to add an user's expense
+router.post('/:id/addExpense', async(req, res) => {
+    const { expenseType, expenseName, expenseDescription, dateofExpense, isRecurring, bankAccountName } = req.body;
 
+    // Get userid from url
+    const studentId = parseInt(req.params.id);
+
+    try {
+        // Validate date inputs
+        if (!dateofExpense) {
+            return res.status(400).send('Enter a valid date!');
+        }
+
+        // Parse and format dates
+        const expenseDate = new Date(Date.parse(dateofExpense)).toISOString();
+
+        // Find bank account be accountName and studentId
+        const bankAccount = await prisma.account.findFirst({
+            where: {
+                accountName: bankAccountName,
+                studentId
+            }
+        })
+
+        await prisma.expense.create({
+            data: {
+                studentId,
+                accountId: bankAccount.id,
+                expenseType,
+                expenseName,
+                expenseDescription,
+                expenseDate,
+                isRecurring,
+            }
+        })
+    } catch(err) {
+        return res.status(500).send('Server error!');
+    }
+
+    res.status(200).send('Great! New expense has been added!');
+})
 
 module.exports = router;
