95df86e3c14cf83a142cd58a829468e144e5b323
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-03 15:18:39 -0700

    Routes to update expense details and delete expense details
    Get updated expense data from request body
    
    Validate date inputs and convert to ISO 8601 format
    
    Check if bank account exists under that user to prevent access to different user's bank account
    
    I f all checks pass, update expense with new information.
    
    For delete, check if expense is an expense of that specific student
    
    If delete expense using expense id

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -96,4 +96,99 @@ router.get('/:id/expensesInfo/account', async (req, res) => {
     }
 })
 
+// Route to update an expense
+router.put('/:id/updateExpense', async (req, res) => {
+    const { expenseId, expenseType, expenseName, expenseDescription, dateofExpense, isRecurring, bankAccountId } = req.body;
+    const studentId = parseInt(req.params.id);
+    // const expenseId = parseInt(req.params.expenseId);
+
+    try {
+        // Validate date inputs
+        if (!dateofExpense) {
+            return res.status(400).send('Enter a valid date!');
+        }
+
+        // Parse and format dates
+        const expenseDate = new Date(Date.parse(dateofExpense)).toISOString();
+
+        // Find bank account by accountName and studentId
+        const bankAccount = await prisma.account.findFirst({
+            where: {
+                id: bankAccountId,
+                studentId
+            }
+        })
+
+        if (!bankAccount) {
+            return res.status(404).send('Bank account does not exist!');
+        }
+
+        // Find the expense to update
+        const expense = await prisma.expense.findUnique({
+            where: {
+                id: expenseId,
+                studentId: studentId,
+            }
+        });
+
+        if (!expense) {
+            return res.status(404).send('Expense does not exist!');
+        }
+
+        // Update the student expense
+        const updatedExpense = await prisma.expense.update({
+            where: {
+                id: expenseId
+            },
+            data: {
+                id: expenseId,
+                accountId: bankAccountId,
+                expenseType,
+                expenseName,
+                expenseDescription,
+                expenseDate,
+                isRecurring,
+            }
+        });
+
+        return res.json(updatedExpense);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error!');
+    }
+});
+
+// Route to delete a student expense
+router.delete('/:id/deleteExpense', async (req, res) => {
+    const { expenseId } = req.body;
+    const studentId = parseInt(req.params.id);
+
+    try {
+       // Find expense specified by expenseId
+        const expense = await prisma.expense.findUnique({
+            where: {
+                studentId,
+                id: expenseId,
+            }
+        })
+
+        if (!expense) {
+            return res.status(404).send('That expense does not exist!');
+        };
+
+        await prisma.expense.delete({
+            where: {
+                id: expenseId,
+            }
+        });
+
+    } catch(err) {
+        console.error(err.message);
+        return res.status(500).send('Server error!');
+    }
+
+    // Success if process completed without errors
+    res.status(200).send('Expense deleted successfully! :)');
+})
+
 module.exports = router;
