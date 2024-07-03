084886963a07488bee487a261b4f1f5b69b68958
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-03 11:48:57 -0700

    Add routes to fetch expenses for students and specified bank accounts
    '/:id/expensesInfo/student' - Retrieves all expenses related to a specific student
    
    '/:id/expensesInfo/account' - Fetches all expenses related to a specific bank account owned by a student
    
    First route gets all expenses details only using the student id parameter in the endpoint URL
    
    Second GET route receives a bank account name from the body and uses it to query the account table and get the uniquebank account id. Then uses the accountId to get all expenses related to that account

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -47,4 +47,53 @@ router.post('/:id/addExpense', async(req, res) => {
     res.status(200).send('Great! New expense has been added!');
 })
 
+// Route to get all student expenses
+router.get('/:id/expensesInfo/student', async (req, res) => {
+    const studentId = parseInt(req.params.id);
+
+    try {
+        // Get all student related expenses
+        const studentExpenses = await prisma.expense.findMany({
+            where: {
+                studentId
+            }
+        });
+
+        return res.json(studentExpenses);
+    } catch (err) {
+        return res.status(500).send('Server error!')
+    }
+
+})
+
+// Route to get all bank account related expenses
+router.get('/:id/expensesInfo/account', async (req, res) => {
+    const studentId = parseInt(req.params.id);
+
+    // Get a bank account from user
+    const { accountName } = req.body;
+
+    try {
+        // Get the unique bank account id
+        const bankAccount = await prisma.account.findFirst({
+            where: {
+                accountName,
+                studentId
+            }
+        })
+
+        // Get all account related expenses
+        const bankAccountExpenses = await prisma.expense.findMany({
+            where: {
+                accountId: bankAccount.id
+            }
+        })
+
+        // Return json object of account expenses
+        return res.json(bankAccountExpenses);
+    } catch (err) {
+        return res.status(500).send('Server error mate!');
+    }
+})
+
 module.exports = router;
