383ad4bf924f6acdf3c909fa474264420417f121
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-06 21:46:50 -0700

    Enhanced Sidebar UI with styled ProfileSection and logout button
    - Integrated the ProfileSection subcomponent into the Sidebar component.
    
    - Styled the Sidebard with a fixed width, dark grey background color and flex-column for all its subcomponents.
    
    - Added a styled logout button to the Sidebar.
    
    - Imported and used a profile image in the profileSection.
    
    - Implemented the ProfileSection with a user image, name, and email display.

--- client/finme/src/App.jsx
+++ client/finme/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignIn from './signIn/SignIn';
 import SignUp from './signUp/SignUp';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +11,10 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' Component={SignIn} /> {/* Default route*/}
         <Route path='/login' Component={SignIn} />
         <Route path='/signup' Component={SignUp} />
-        <Route path='/' Component={SignIn} /> {/* Default route*/}
+        <Route path='/dashboard' Component={Dashboard} />
       </Routes>
 
     </Router>
--- client/finme/src/Dashboard/Sidebar/Sidebar.jsx
+++ client/finme/src/Dashboard/Sidebar/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import ProfileSection from './ProfileSection';
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='w-1/4 bg-gray-900 text-white p-6 flex flex-col justify-between'>
         <div>
-            {/* Profile Section Here */}
+            <ProfileSection />
             {/* Navigation Links Section Here */}
         </div>
-        {/* Log out button here */}
+        <button className='mt-6 bg-red-500 px-4 py-2 rounded-lg'>Log Out</button>
     </div>
   )
 }
