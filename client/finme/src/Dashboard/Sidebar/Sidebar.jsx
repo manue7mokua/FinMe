eaa2b0bf401ac9fe9ea438ae70e0d52f274e0b7a
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-06 21:54:09 -0700

    Add NavigationLinks component for Sidebar Navigation
    - Created NavigationLinks component with a list of menu items.
    
    - Styled the navigation links with color differentiation to represent active and inactive states for different pages.

--- client/finme/src/Dashboard/Sidebar/Sidebar.jsx
+++ client/finme/src/Dashboard/Sidebar/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProfileSection from './ProfileSection';
+import NavigationLinks from './NavigationLinks';
 
 const Sidebar = () => {
   return (
     <div className='w-1/4 bg-gray-900 text-white p-6 flex flex-col justify-between'>
         <div>
             <ProfileSection />
-            {/* Navigation Links Section Here */}
+            <NavigationLinks />
         </div>
         <button className='mt-6 bg-red-500 px-4 py-2 rounded-lg'>Log Out</button>
     </div>
