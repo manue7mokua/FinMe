baada1253393497bde16ce1f2e85bfece5bd86d2
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-09 11:53:23 -0700

    Styled CreatedCard with static details
    - Added mo
    ck account details to CreateCard component to guide styling display
    
    - Added two SVG assets to impro
    ve the UI visuals of the created card for the user.
    
    - Imported CreateCard component to Wallets comp
    onent to be reused for making different cards via props.
    
    -Styled div containing card with flex-row
    and a gap of 10px to separate the cards and improve the visual display of the list of cards when they ar
    e added by the user to the wallets page.

--- client/finme/src/Wallets/Wallets.jsx
+++ client/finme/src/Wallets/Wallets.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Sidebar from '../Dashboard/Sidebar/Sidebar';
+import CreateCard from './CreateCard';
 
 const Wallets = () => {
   return (
     <div className='h-screen flex bg-black'>
         <Sidebar />
+        <div className='flex flex-row gap-10 p-20 items-start justify-center'>
+           <CreateCard />
+        </div>
+
     </div>
   )
 }
