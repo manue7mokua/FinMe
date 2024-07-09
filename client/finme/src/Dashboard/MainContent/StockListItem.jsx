9afc5c53894549499e517ac25ec44b88ce044130
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-08 23:45:49 -0700

    Created new StockListItem subcomponent that is reusable
    - Developed a new React component  to display individual stock information including company icon, name, abbreviation, stock price and performance indicators.
    
    - Added PropTypes for type-checking to enhance component reliability and maintanability.
    
    - Utilized custom font styles and layouts to ensure visual clarity in displaying key stock price information in an easily digestible format.

---
+++ client/finme/src/Dashboard/MainContent/StockListItem.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StockListItem = ({ companyIcon, companyName, companyAbbrev, stockPrice, performancePercentage }) => {
+  return (
+    <div className='inline-flex items-center gap-[221px] relative'>
+        <div className='w-[62px] items-center gap-1.5 flex relative'>
+            <img src={companyIcon} alt={`${companyName} icon`} className='relative w-6 h-6' />
+            <p className='relative w-fit mt-[-1.00px] mr-[-63.00px] [font-family:"Everett-Medium", Helvetica] font-medium text-transparent text-xs tracking-[0] leading-normal'>
+                <span className='text-[#2c2c2c]'>
+                    {companyName}
+                    <br/>
+                </span>
+                <span className='text-[#828282]'>{companyAbbrev}</span>
+            </p>
+        </div>
+        <div className='flex flex-col w-[33px] items-end justify-center relative'>
+            <div className='relative w-fit mt-[-11.00px] [font-family:"Everett-Regular", Helvetica] font-normal text-[#2c2c2c] text-xs text-right tracking-[0] leading-normal whitespace-nowrap'>
+                {stockPrice}
+            </div>
+            <div className='relative w-fit [font-family:"Everett-Regular", Helvetica] font-normal text-[#77b900] text-xs text-right tracking-[0] leading-normal whitespace-nowrap'>
+                {performancePercentage}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+StockListItem.propTypes = {
+    companyName: PropTypes.string,
+    companyAbbrev: PropTypes.string,
+    stockPrice: PropTypes.string,
+    performancePercentage: PropTypes.string
+}
+
+export default StockListItem;
