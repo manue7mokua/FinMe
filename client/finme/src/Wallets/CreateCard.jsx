e094beec8157e3363570696330b8bbb567924b50
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-09 14:27:14 -0700

    Styled CreateCard components, created multiple card items o n Wallets component
    - Added a styling color variable  to tailwind.config.js to extend the
    existing tailwing color paletter for my UI.
    
    - Structured the multiple cards display for all the created c
    ards to flex-row and flex-wrap to allow the cards to stack side by side and top-down when displayed on the scr
    een.
    
    - Positioned all the created cards at the center of the screen.

--- client/finme/src/Wallets/CreateCard.jsx
+++ client/finme/src/Wallets/CreateCard.jsx
@@ -12,13 +12,13 @@ const CreateCard = ({
     <div className='relative flex flex-col w-[352px] h-[267px] items-start gap-2'>
         <div className={`w-full flex flex-col items-start justify-start pt-0 px-0 pb-[35px] box-border gap-[8px] leading-[normal] tracking-[normal] text-left text-3xl text-gray-02 font-bold-16-24`}>
       <h3 className="m-0 relative text-inherit leading-[32px] font-normal font-inherit">
-        Bank of America
+        {bankAccountName}
       </h3>
       <section className="self-stretch shadow-[0px_20px_25px_rgba(76,_103,_100,_0.1)] rounded-lg bg-white flex flex-col items-start justify-start py-5 px-6 text-left text-3xl text-default-black font-bold-16-24">
         <div className="self-stretch flex flex-col items-start justify-start gap-[12px]">
           <div className="self-stretch flex flex-row items-center justify-between pt-0 px-0 pb-2.5 gap-[20px] border-b-[1px] border-solid border-gray-06">
             <div className="relative leading-[32px] capitalize font-extrabold inline-block min-w-[80px] whitespace-nowrap">
-              $2,549
+              {bankAccountBalance}
             </div>
             <a className="[text-decoration:none] relative text-sm leading-[20px] capitalize font-medium text-secondary text-right inline-block min-w-[54px]">
               Balance
@@ -27,13 +27,13 @@ const CreateCard = ({
           <div className="self-stretch rounded bg-darkgoldenrod flex flex-row items-center justify-between p-4 gap-[8px] text-sm text-special-bg2">
             <div className="flex flex-col items-start justify-start">
               <div className="w-[120px] relative leading-[20px] capitalize font-medium inline-block">
-                Account type
+                {bankAccountType}
               </div>
               <b className="w-[120px] relative text-base leading-[24px] capitalize inline-block text-white">
                 Debit Card
               </b>
               <div className="relative leading-[20px] capitalize font-medium">
-                **** **** **** 2598
+                {bankCardNumber}
               </div>
             </div>
             <div className="w-32 flex flex-col items-end justify-center gap-[12px] text-right text-base text-white">
@@ -47,7 +47,7 @@ const CreateCard = ({
               </div>
               <div className="self-stretch flex flex-row items-center justify-start gap-[12px]">
                 <b className="flex-1 relative leading-[24px] capitalize whitespace-nowrap">
-                  $2,549
+                  {bankAccountBalance}
                 </b>
                 <div className="h-6 w-6 rounded-[20px] bg-white flex flex-row items-start justify-start p-1 box-border">
                   <img
--- client/finme/src/Wallets/Wallets.jsx
+++ client/finme/src/Wallets/Wallets.jsx
@@ -6,10 +6,34 @@ const Wallets = () => {
   return (
     <div className='h-screen flex bg-black'>
         <Sidebar />
-        <div className='flex flex-row gap-10 p-20 items-start justify-center'>
-           <CreateCard />
+        <div className='flex items-center justify-around'>
+            <div className='flex flex-row gap-10 p-20 items-center justify-evenly flex-wrap'>
+            <CreateCard
+                bankAccountName = 'Bank of America'
+                bankAccountBalance = '$22,509'
+                bankAccountType = 'Checking Account'
+                bankCardNumber = '**** **** **** 7158'
+            />
+            <CreateCard
+                bankAccountName = 'Chase Bank'
+                bankAccountBalance = '$516'
+                bankAccountType = 'Credit Card'
+                bankCardNumber = '**** **** **** 0692'
+            />
+            <CreateCard
+                bankAccountName = 'Citi Bank'
+                bankAccountBalance = '$7,419'
+                bankAccountType = 'Savings Account'
+                bankCardNumber = '**** **** **** 1267'
+            />
+            <CreateCard
+                bankAccountName = 'Bank of America'
+                bankAccountBalance = '$1,288'
+                bankAccountType = 'Checking Account'
+                bankCardNumber = '**** **** **** 2164'
+            />
+            </div>
         </div>
-
     </div>
   )
 }
