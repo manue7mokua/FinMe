e1e6f56dffad08d17a2e39addc871e35ac98e314
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-09 17:13:10 -0700

    Test display of IncomeListItem component
    - Added static data to IncomeListItem component to test the display structure of the various values in and indiviudal list component.

--- client/finme/src/Dashboard/MainContent/IncomeListItem.jsx
+++ client/finme/src/Dashboard/MainContent/IncomeListItem.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
-const IncomeListItem = () => {
+const IncomeListItem = ({
+  incomeName,
+  incomeDateRange,
+  incomeAmount,
+  incomeCurrencyDenomination
+}) => {
   return (
-    <div>
-
-    </div>
+    <div className="w-[427px] h-[58px] top-[155px] absolute left-0 rounded-[10px]">
+          <div className="absolute w-96 h-[57px] top-0 left-0 overflow-hidden">
+            <div className="relative w-[1100px] h-[60px]">
+              <div className="absolute w-[1100px] h-[60px] top-0 left-0">
+                <div className="relative w-96 h-[50px] top-[7px]">
+                  <div className="absolute w-5 h-5 top-[13px] left-5 rounded-[3px] border-[1.5px] border-solid border-fontsprimary-icon" />
+                </div>
+              </div>
+              <div className="absolute w-[200px] top-2.5 left-[105px] [font-family:'Inter-Medium',Helvetica] font-medium text-fontsprimary text-sm tracking-[0] leading-[normal]">
+                Bananas
+              </div>
+              <div className="absolute w-[200px] top-8 left-[105px] [font-family:'Inter-Regular',Helvetica] font-normal text-fontsprimary-variant text-sm tracking-[0.70px] leading-[normal]">
+                May, 4 - June, 5
+              </div>
+              <div className="absolute w-[15px] h-[15px] top-[23px] left-[60px] bg-[url(/union.svg)] bg-[100%_100%]" />
+              <div className="w-20 top-1.5 left-72 [font-family:'Inter-Medium',Helvetica] font-medium text-fontsprimary text-sm tracking-[0] absolute text-right leading-[normal]">
+                $200
+              </div>
+              <div className="top-[31px] left-[281px] absolute w-20 [font-family:'Inter-Regular',Helvetica] font-normal text-fontsprimary-variant text-xs text-right tracking-[0.60px] leading-[normal]">
+                USD
+              </div>
+            </div>
+          </div>
+          <div className="w-96 h-px top-[57px] bg-[#d9d5ec] absolute left-0" />
+        </div>
   )
 }
 
