a459a4d7b3045d1659eb6e5855ab6b5a423abe28
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 14:54:15 -0700

    Style ExpensesHeader component with mock data
    - Setup mock data to display visual representation of ExpensesHeader component with dummy data.
    
    - Added width property of  to fit the width of all content displayed in the component.
    
    - Added setup for icons to show different expense categories.
    
    - Styled the heading of the component with a bold font.

--- client/finme/src/Expenses/ExpensesHeader.jsx
+++ client/finme/src/Expenses/ExpensesHeader.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const ExpensesHeader = () => {
   return (
-    <div>
-
+    <div className=''>
+      <div className='flex flex-row p-5 bg-white w-fit'>
+      <div className="flex flex-col items-start p-5 bg-white">
+        <h1 className="text-2xl font-bold">Expenses</h1>
+        <p className="mt-2">01 - 25 March, 2020</p>
+      </div>
+      <div className="flex gap-2 mt-3">
+          <img src="icon1.png" alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src="icon2.png" alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src="icon3.png" alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src="icon4" alt="category-icon" className="w-10 h-10 rounded-full" />
+        </div>
+      </div>
     </div>
   )
 }
