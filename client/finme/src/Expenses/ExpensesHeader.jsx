49fb836016c14325727c86c9906fadd0e752cb6e
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 15:01:50 -0700

    Imported and displayed icons for different expense categories.
    - Imported different expense category icons from assets folder.
    
    - Used the icon import as image sources for my img elements in the ExpensesHeader component.

--- client/finme/src/Expenses/ExpensesHeader.jsx
+++ client/finme/src/Expenses/ExpensesHeader.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import busicon from '../assets/busicon.svg';
+import houseicon from '../assets/houseicon.svg';
+import addnewicon from '../assets/addicon.svg';
+import foodicon from '../assets/foodicon.svg';
 
 const ExpensesHeader = () => {
   return (
@@ -9,10 +13,10 @@ const ExpensesHeader = () => {
         <p className="mt-2">01 - 25 March, 2020</p>
       </div>
       <div className="flex gap-2 mt-3">
-          <img src="icon1.png" alt="category-icon" className="w-10 h-10 rounded-full" />
-          <img src="icon2.png" alt="category-icon" className="w-10 h-10 rounded-full" />
-          <img src="icon3.png" alt="category-icon" className="w-10 h-10 rounded-full" />
-          <img src="icon4" alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src={houseicon} alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src={busicon} alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src={foodicon} alt="category-icon" className="w-10 h-10 rounded-full" />
+          <img src={addnewicon} alt="category-icon" className="w-10 h-10 rounded-full" />
         </div>
       </div>
     </div>
