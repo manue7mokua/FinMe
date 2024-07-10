4b0e0fbde063a19d0a19b4b164f5f56fddc23c8e
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 14:16:07 -0700

    New folder for Expenses page components
    - Divide /expenses page into 5 components (WeeklyExpenses, ExpensesChart, Expenses, ExpenseItem and CategoryWeighting)
    
    - Set up code layout for each newly created component in Expenses folder.

---
+++ client/finme/src/Expenses/CategoryWeighting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryWeighting = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryWeighting
---
+++ client/finme/src/Expenses/WeeklyExpenses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeeklyExpenses = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WeeklyExpenses
