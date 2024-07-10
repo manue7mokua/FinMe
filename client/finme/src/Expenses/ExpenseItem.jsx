f23481a5df4e0f2e01438d5bf717132fc70239f0
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 15:43:06 -0700

    ExpenseItem component code setup
    - Setup code layout for the ExpenseItem component.
    
    - Purpose of this component is to display various user expenses and details about the expense including its date, amount and category.
    
    - Component takes in 5 props containing expense item data (categoryIcon, categoryTitle, expenseDate, expenseDay and expenseAmount)

---
+++ client/finme/src/Expenses/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ExpenseItem = ({
+    categoryIcon,
+    categoryTitle,
+    expenseDate,
+    expenseDay,
+    expenseAmount
+}) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ExpenseItem;
