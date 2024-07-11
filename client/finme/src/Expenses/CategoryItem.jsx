b9ed9afbe1925291e8c8bf2dbd8a83bcb1721de2
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 18:01:33 -0700

    Create html elements for category name and amount in Category Item component.
    - Styled category name and amounts with flex-row direction.
    
    Added names 'category name' and 'category amount' as placeholders for the actual data to be passed in.

--- client/finme/src/Expenses/CategoryItem.jsx
+++ client/finme/src/Expenses/CategoryItem.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const CategoryItem = () => {
   return (
-    <div>
+    <div className="flex justify-between items-center p-5 border-b border-gray-200">
+        <div className='flex flex-row justify-between'>
+            <p className='font-bold'>category name</p>
+            <span className="text-right block mt-1">category amount</span>
+        </div>
 
     </div>
   )
