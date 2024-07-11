56406100f4ce3e160602f2272687cc8359b04209
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 18:13:47 -0700

    Passing data as props to the CategoryItem component
    - Passing data as props which are then used to render the UI elements for each category item, including the category name, amount and the percentage progress bar.

--- client/finme/src/Expenses/CategoryItem.jsx
+++ client/finme/src/Expenses/CategoryItem.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-const CategoryItem = () => {
+const CategoryItem = ({
+    categoryName,
+    categoryAmount,
+    percentageWidth
+}) => {
   return (
     <div className="flex justify-between items-center p-5 border-b border-gray-200">
         <div className='flex flex-row justify-between'>
-            <p className='font-bold'>category name</p>
-            <span className="text-right block mt-1">category amount</span>
+            <p className='font-bold'>{categoryName}</p>
+            <span className="text-right block mt-1">{categoryAmount}</span>
         </div>
         <div className="w-full bg-gray-300 rounded-full h-2.5 mt-2">
-            <div className="bg-green-500 h-2.5 rounded-full" style={{ width: item.width }}></div>
+            <div className="bg-green-500 h-2.5 rounded-full" style={{ width: percentageWidth }}></div>
         </div>
     </div>
   )
 }
 
 export default CategoryItem;
+
