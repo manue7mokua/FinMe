80bd5571ff24111925b8174860fe04048300f005
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-11 16:32:18 -0700

    Fetch company stock exchange symbol data and cache locally
    - Get company symbol and name data from financial modelling prep API.
    
    - After getting data, create JSON file containing all company symbol data in stocksAPI directory.
    
    - Company symbol data will be used to provide user suggestions about a company to add to their stock watchlist.

---
+++ server/routes/stocksAPI/fetchCompanySymbols.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+async function fetchAndCacheCompanySymbols() {
+    const apiKey = process.env.FINME_FMP_API_KEY;
+    const url = `https://financialmodelingprep.com/api/v3/stock/list?apikey=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        const companies = response.data;
+
+        if (response) {
+            // Cache the company symbols
+            const cacheFilePath = path.join(__dirname, 'companySymbols.json');
+            fs.writeFileSync(cacheFilePath, JSON.stringify(companies, null, 2));
+        } else {
+            return ("Error creating file with data")
+        }
+
+    } catch (error) {
+        return ('Error fetching company symbols:', error);
+    }
+}
+
+fetchAndCacheCompanySymbols();
