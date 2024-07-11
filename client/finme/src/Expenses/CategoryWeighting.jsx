8474fca31d1811df41735069eaaef87de72ee0cb
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 18:22:35 -0700

    Create array with all categories mock data
    - Created a new array 'allCategories' containing objects with different category expense data.

--- client/finme/src/Expenses/CategoryWeighting.jsx
+++ client/finme/src/Expenses/CategoryWeighting.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const CategoryWeighting = () => {
+    const allCategories = [
+        { categoryName: 'Food and Drinks', categoryAmount: '872.40', percentageWidth: '80%' },
+        { categoryName: 'Shopping', categoryAmount: '1,378.20', percentageWidth: '90%' },
+        { categoryName: 'Housing', categoryAmount: '928.50', percentageWidth: '70%' },
+        { categoryName: 'Transportation', categoryAmount: '420.70', percentageWidth: '50%' },
+        { categoryName: 'Car', categoryAmount: '520.00', percentageWidth: '60%' },
+      ];
+
   return (
     <div className='bg-gray-200 p-5 rounded-lg'>
         <h2 className='text-xl font-bold mb-4'>Where your money goes?</h2>
