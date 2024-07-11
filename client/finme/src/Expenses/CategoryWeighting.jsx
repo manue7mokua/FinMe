fea8c30667efd548d7de589d80a2a97a7c43141e
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 18:18:25 -0700

    Add heading and background color to CategoryWeighting component
    - Set background color for CategoryWeighting component to a shade of gray .
    
    Add heading title  with a bold font and bottom margin of 4px.

--- client/finme/src/Expenses/CategoryWeighting.jsx
+++ client/finme/src/Expenses/CategoryWeighting.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const CategoryWeighting = () => {
   return (
-    <div>
-
+    <div className='bg-gray-200 p-5 rounded-lg'>
+        <h2 className='text-xl font-bold mb-4'>Where your money goes?</h2>
+        
     </div>
   )
 }
