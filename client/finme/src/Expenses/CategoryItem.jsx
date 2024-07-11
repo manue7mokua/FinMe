477d809bd3ccc6f2047c895d4aa525a08147860d
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 17:46:52 -0700

    New CategoryItem component to handle creation of new categories
    - Setup react component layout for CategoryItem component.

--- client/finme/src/Expenses/CategoryItem.jsx
+++ client/finme/src/Expenses/CategoryItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CategoryWeighting = () => {
+const CategoryItem = () => {
   return (
     <div>
 
@@ -8,4 +8,4 @@ const CategoryWeighting = () => {
   )
 }
 
-export default CategoryWeighting;
+export default CategoryItem;
