93263bb92e11872eb15e71239a141eec7fe649d6
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-10 17:35:06 -0700

    Create arrays containing weekly mock data
    - expensesThisWeek array containing object of expense details for the current week.
    
    - expensesLastWeek array containing object of expense details from previous week.

--- client/finme/src/Expenses/WeeklyExpenses.jsx
+++ client/finme/src/Expenses/WeeklyExpenses.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 
 const WeeklyExpenses = () => {
+  const expensesThisWeek = [
+    { categoryIcon: '🛒', categoryTitle: 'Grocery', expenseDate: '2:15 pm', expenseDay: 'Sunday', expenseAmount: '-326.80' },
+    { categoryIcon: '🚗', categoryTitle: 'Transportation', expenseDate: '9:12 am', expenseDay: 'Friday', expenseAmount: '-15.00' },
+    { categoryIcon: '💡', categoryTitle: 'Utilities', expenseDate: '5:47 pm', expenseDay: 'Thursday', expenseAmount: '-185.75' }
+  ];
+
+  const expensesLastWeek = [
+    { categoryIcon: '🍽️', categoryTitle: 'Food and Drink', expenseDate: '5:12 pm', expenseDay: 'Friday', expenseAmount: '-156.00' },
+    { categoryIcon: '🎬', categoryTitle: 'Entertainment', expenseDate: '5:12 pm', expenseDay: 'Saturday', expenseAmount: '-35.20' }
+  ];
   return (
-    <div className="p-5">
+    <div>
 
     </div>
   )
