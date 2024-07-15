be70815066b83e9f43b7e03068ec25d64a9b9297
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-15 01:56:51 -0700

    Update the NavigationLinks component to use the Link component from react-router-dom

--- client/finme/src/Dashboard/Sidebar/NavigationLinks.jsx
+++ client/finme/src/Dashboard/Sidebar/NavigationLinks.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavigationLinks = () => {
   return (
     <nav className='w-[119px] h-[292px] relative flex flex-col justify-evenly pt-20 '>
-        <div className='text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'><a href='#'>Dashboard</a></div>
-        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'><a href='#'>Expenses</a></div>
-        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'><a href='#'>Wallets</a></div>
-        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'><a href='#'>Incomes</a></div>
-        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'><a href='#'>Plan</a></div>
-        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'><a href='#'>Settings</a></div>
+        <div className='text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'>
+          <Link to='/dashboard'>Dashboard</Link>
+        </div>
+        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'>
+          <Link to='/expenses'>Expenses</Link>
+        </div>
+        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'>
+          <Link to='/wallets'>Wallets</Link>
+        </div>
+        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'>
+          <Link to='/incomes'>Incomes</Link>
+        </div>
+        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'>
+          <Link to='/plan'>Plan</Link>
+        </div>
+        <div className='opacity-50 text-white text-[25px] font-semibold font-["Poppins"] leading-[35px] mb-4'>
+          <Link to='/settings'>Settings</Link>
+        </div>
     </nav>
   )
 }
 
 export default NavigationLinks;
+
