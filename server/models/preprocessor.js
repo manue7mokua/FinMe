fdb6d73f0a0a131e243041f29159c13d16a39fe3
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-16 11:53:20 -0700

    Convert text to lowercase and splitting it into individual words using a regex pattern
    - Using the (\W+) regex pattern to match one or more non-word characters.

--- server/models/preprocessor.js
+++ server/models/preprocessor.js
@@ -2,6 +2,8 @@ const stopword = require('stopword');
 const stemmer = require('stemmer');
 
 function preprocess(text) {
+  // Convert text to lowercase and split into words
+  let words = text.toLowerCase().split(/\W+/);
   
 }
 
