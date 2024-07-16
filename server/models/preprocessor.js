c6c8d0dd1ae07f1332199da1f45c4ef786b772c9
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-16 11:54:15 -0700

    Use stopword and stemmer libraries to remove common stop words and stem remaining words

--- server/models/preprocessor.js
+++ server/models/preprocessor.js
@@ -4,7 +4,11 @@ const stemmer = require('stemmer');
 function preprocess(text) {
   // Convert text to lowercase and split into words
   let words = text.toLowerCase().split(/\W+/);
-  
+  // Remove stop words
+  words = stopword.removeStopwords(words);
+  // Stem the words
+  words = words.map(word => stemmer(word));
+  return words;
 }
 
 module.exports = preprocess;
