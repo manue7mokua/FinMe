e8066a267c3e8283e9bb83d5388d2377faf150bc
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-16 13:45:19 -0700

    Defining the preprocessDataset function
    - This function takes in the dataset JSON file and generates feature vectors and labels for each word in the dataset.
    
    - Then uses the preprocessor function to preprocess each word and generates a feature vector by setting the corresponding category index to 1.

--- server/models/preprocessDataset.js
+++ server/models/preprocessDataset.js
@@ -1,2 +1,36 @@
 const preprocessor = require('./preprocessor');
 const dataset = require('../data/dataset.json');
+
+const preprocessDataset = () => {
+    const categories = ["Food", "Entertainment", "Transport", "Bills", "Personal"];
+    const wordToCategory = {};
+  
+    Object.keys(dataset).forEach(category => {
+      dataset[category].forEach(word => {
+        wordToCategory[word] = category;
+      });
+    });
+  
+    const features = [];
+    const labels = [];
+  
+    Object.keys(dataset).forEach((category, categoryIndex) => {
+      dataset[category].forEach(word => {
+        const preprocessedWord = preprocess(word);
+        const featureVector = Array(categories.length).fill(0);
+  
+        preprocessedWord.forEach(stemmedWord => {
+          const wordCategory = wordToCategory[stemmedWord];
+          if (wordCategory) {
+            const index = categories.indexOf(wordCategory);
+            featureVector[index] = 1;
+          }
+        });
+  
+        features.push(featureVector);
+        labels.push(categoryIndex);
+      });
+    });
+  
+    return { features, labels, categories };
+  }
