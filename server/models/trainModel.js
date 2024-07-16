1843948ca3dd1752b4c33245b737808d39ffd284
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-16 13:53:33 -0700

    Defining the trainModel function
    - The trainModel function takes no arguments and first calls the preprocessDataset function to get the preprocessed dataset which includes feature vectors and labels.
    
    - Creates a new instance of the LinearRegression class and trains the model using the train method passing in the features and labels then returns the model.

--- server/models/trainModel.js
+++ server/models/trainModel.js
@@ -1,2 +1,11 @@
 const LinearRegression = require('./linearRegression');
 const preprocessDataset = require('./preprocessDataset');
+
+const trainModel = () => {
+    const { features, labels } = preprocessDataset();
+    const model = new LinearRegression();
+    model.train(features, labels);
+    return model;
+  }
+
+module.exports = trainModel;
