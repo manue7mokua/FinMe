ea379609a0ff3f9d1e8153cb65e1569569126f04
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-23 14:00:14 -0700

    Enhance sidebar styling and add hover effects
    - Highlight the active page in the sidebar using the Navlink and conditional class names.
    
    - Added hover effect to change the navigation link color to orange-400.
    
    - Ensured navigation links are styled in a flex-column layout for consistent alignment.
    
    - Updated the Sidebar and ProfileSection components with a black background and flex-column layout.
    
    - Styled the email to have an overflow-wrap for better handling of long text.

--- client/finme/src/Dashboard/Sidebar/ProfileSection.jsx
+++ client/finme/src/Dashboard/Sidebar/ProfileSection.jsx
@@ -3,10 +3,12 @@ import profileImage from '../../assets/profile.jpg';
 
 const ProfileSection = () => {
   return (
-    <div className='flex flex-col items-start space-x-4 justify-start'>
-            <img src={profileImage} alt='profile image' className='w-[100px] h-[120px] rounded-full' />
-            <h2 className='text-white text-3xl font-semibold font-["Poppins"] leading-[35px] mt-2'>Manuel</h2>
-            <p className='opacity-60 text-white text-[17px] font-normal font-["Poppins"] leading-[27px]'>crackedeman@gmail.com</p>
+    <div className='flex flex-col items-center justify-center bg-black p-4 rounded-lg'>
+      <img src={profileImage} alt='profile' className='w-[100px] h-[120px] rounded-full' />
+      <h2 className='text-white mt-10 text-3xl font-semibold font-["Poppins"] leading-[35px] mt-2'>Manuel</h2>
+      <p className='opacity-60 mt-5 text-white text-[17px] font-normal font-["Poppins"] leading-[27px] overflow-wrap break-words'>
+        crackedeman@gmail.com
+      </p>
     </div>
   )
 }
--- client/finme/src/Dashboard/Sidebar/Sidebar.jsx
+++ client/finme/src/Dashboard/Sidebar/Sidebar.jsx
@@ -4,15 +4,13 @@ import NavigationLinks from './NavigationLinks';
 
 const Sidebar = () => {
   return (
-    <div className='w-1/6 bg-gray-900 text-white p-6 flex flex-col justify-between'>
-        <div className='w-[119px] flex flex-col'>
-            <ProfileSection />
-            <NavigationLinks />
-            <button className='mt-6 bg-red-500 px-4 py-2 rounded-lg'>Log Out</button>     
-        </div>
-
+    <div className='w-1/6 bg-black text-white p-6 flex flex-col justify-between'>
+      <ProfileSection />
+      <NavigationLinks />
+      <button className='mt-6 bg-red-500 px-4 py-2 rounded-lg hover:bg-red-800'>Log Out</button>     
     </div>
   )
 }
 
 export default Sidebar;
+
