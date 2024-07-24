9ad18ddc0166993c2b7be93deda8312616fcf627
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-07-23 17:05:31 -0700

    Implemented and verified cron job for updating stock prices
    - Added the cron job to my server.js file to update the stock prices every hour.
    
    - Scheduled the cron job to run every hour and added logging to berify execution.
    
    - Made an API call to the Financial Modelling Prep API to update stock prices and performance percentage in the database.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const accountRoutes = require('./routes/bankdetails.js');
 const incomeRoutes = require('./routes/incomes.js');
 const expenseRoutes = require('./routes/expenses.js');
 const stockData = require('./routes/stocksAPI/fetchCompanyStockData.js');
+var cron = require('node-cron');
+const updateStockPrices = require('./routes/stocksAPI/updateStockPrices.js')
 const PORT = 5000;
 
 app.use(cors())
@@ -26,5 +28,10 @@ app.use('/expenses', expenseRoutes);
 
 // Get stock data
 app.use('/api', stockData);
+
+// Schedule the cron job to run every hour
+cron.schedule('0 * * * *', async () => {
+    await updateStockPrices();
+});
 
 app.listen(PORT);
