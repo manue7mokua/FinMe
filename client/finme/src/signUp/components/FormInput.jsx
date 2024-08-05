48dd9412a04d75cb3029d5881c79695b833f9348
Emmanuel Onchweri Mokua <manue7mokua@meta.com>
2024-08-05 08:31:01 -0700

    Changed form input colors

--- client/finme/src/signUp/components/FormInput.jsx
+++ client/finme/src/signUp/components/FormInput.jsx
@@ -7,7 +7,7 @@ const FormInput = ({ type, placeholder, value, onChange }) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500'
+      className='w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-700'
     />
   );
 };
